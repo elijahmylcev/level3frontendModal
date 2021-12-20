7386b2e3482da1446ef07a7db34b2c373dc1ac42
Elijah Mylcev <elijahmylcev@gmail.com>
2021-12-20 19:22:03 +0300

    Configuration

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+console.log('IsDev value: ', isDev);
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [new CssMinimizerPlugin(), new TerserWebpackPlugin()];
+  }
+
+  return config;
+};
+
+const filename = ext => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './js/index.js',
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'docs'),
+    assetModuleFilename: '[hash][ext][query]',
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    // hot: isDev
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(s[ac]ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: [/node_modules/, /index.test.js/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
