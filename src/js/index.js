be6ba4de2dde0b4cdf49cc3dfcfe58fd90dc6ca5
Elijah Mylcev <elijahmylcev@gmail.com>
2021-12-21 23:05:02 +0300

    working function for modal

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 import 'reset-css';
 import '../style.scss';
+
+function modal(triggerSelectorOpen, modalSelector, triggerSelectorClose) {
+  const bntOpen = document.querySelector(triggerSelectorOpen);
+  const modal = document.querySelector(modalSelector);
+  const btnClose = document.querySelectorAll(triggerSelectorClose);
+
+  bntOpen.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
+
+  btnClose.forEach(btn => {
+    btn.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+  });
+}
+
+modal('.trigger', '#exampleModal', '[data-close]');
